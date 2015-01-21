1cf3dd45fb9146315a056bd9baf5e27afff5383a
Davor Petrovic <extra_shakale@hotmail.com>
2015-01-21 21:04:13 +0100

    prvi commit

---
+++ Projekat (fix)/DOING IT AGAIN/js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$(".circle").on('click',function(){
+		$(".outer_circle").animate({
+			width: "186px",
+			height: "186px",
+			border:"1px solid #2ea0ff"
+
+		});
+	});
+});
